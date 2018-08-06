446f493bac879e11ad345f42424740f8452aab1f
Leslie Tecumseh <lestec076@gmail.com>
2018-08-06 10:30:52 -0700

    Update app.js
    Took out extra comments

--- js/app.js
+++ js/app.js
@@ -19,10 +19,8 @@ var Enemy = function(x,y,speed) {
     // a helper we've provided to easily load images
     //this.x = 0; //--changing to get bugs own space
     this.x = x;
-    //this.y = 58;//center at 0 its in the water
+    //this.y = 58;//center at 0 bug is in the water
     this.y = y + 58;
-    //3rd parameter to enemy class(Inside constructor,
-    //setting this.speed to value of parameter.)
     this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
@@ -35,7 +33,6 @@ Enemy.prototype.update = function(dt) {
     // all computers.
 
     //check to see if enemy is passed boundary
-    //405 stops at edge, but not smooth transition
     if(this.x < 502) {
         //move 
         //mult the speed by the dt parameter on the x axis 
@@ -60,49 +57,37 @@ Enemy.prototype.render = function() {
 
 class Player {
     constructor() {
-//in engine.js** col(5) * 101 = canvas width 505 -line 140
+//in engine.js** col(5) * 101 = canvas width 505 (line 140)
         this.horz = 101;
 //engine.js** row(6) * 83 = canvas height 606
         this.vert = 83;
         this.sprite = 'images/char-princess-girl.png';
 //startX places player 2 blocks to rt on x axis(col/horz)
         this.startX = this.horz * 2;
-//startY places player 5 blocks down from top row/top left 0,0
-    //may need to use subt to get centered better when move?    
-    //this.startY = (this.vert * 5) - 35; **NEED TO CHANGE TO GET to collide
+//startY places player 5 blocks down from top row/top left 0,0    
+//this.startY = (this.vert * 5) - 35; **NEED TO CHANGE TO GET to collide
         this.startY = (this.vert * 4) + 58;
-
 //set bk to bottom middle with above calc -- use for reset
         this.x = this.startX;
         this.y = this.startY;
-//setting winGame to try to get game to stop moving and modal to show
+//setting winGame to get game to stop moving when game over
         this.winGame = false;
+ }
 
-    }
-//};
-
-/* not sure if this goes here
-player class update()
-Player.prototype.update = function () {
-}
-*/
 //a new method for hero class update position 
-//HAVE TO USE update here for engine.js
     update() {
     //check collision here
         this.checkCollision();
         if (this.y <= 0) {
-            console.log("Win");
-    //setting to try to stop animation after win
+    //setting to stop animation after win, if statement set in engine.js
             this.winGame = true;        
-            //stopGame();
             gmModal();
         }
     }
+
 //modified and help from:
 //https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection  
 //https://stackoverflow.com/questions/2440377/javascript-collision-detection
-    
     checkCollision() {
         //check for collisions with enemies
         for (var i = 0; i < allEnemies.length; i++) {
@@ -118,38 +103,14 @@ Player.prototype.update = function () {
         this.x = this.startX;
         this.y = this.startY;
     }
-   /*
-    for(let enemy of allEnemies) {
-    //did player x and y collide with enemy
-        if (this.y === enemy.y) {
-            console.log("same row");
-        }
-        console.log(this.y, enemy.y)
-    }
-    //check win here?
-        //did player x and y reach final tile?
-*/
-//trying to figure out where to add gameOver(stopGame)
-    /*not working cannot define win, go to engine??
-    stopGame() {
-        if (player.winGame === true) {
-            win.cancelAnimationFrame(id);
-        }
-        else {
-            id = win.requestAnimationFrame(main);
-        }
-    }
-*/
-    
-
-    
 //render: renders the image of the user..Following enemies 
     render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
 //handleInput method: allows users to use arrow keys
-/*see below in keypress fx hanleInput
+/*
+ *see below in keypress fx handleInput
  *update hero's x and y property according to input    
  *@param {string} input - direction to travel
  *
@@ -157,11 +118,12 @@ Player.prototype.update = function () {
  handleInput(input) {
     switch(input) {
         case 'left':
-/*this.x -= 20; adding - makes it go left, without it,
- it does not move correctly, it jumps to the top
-  + sign has it going to the right
-Vise Versa if - is on down it will go up */
-
+/*
+this.x -= 20; adding (-) makes it go left, without it,
+it does not move correctly, it jumps to the top
+  (+) sign has it going to the right
+Vise Versa if - is on down, it will go up 
+*/
 //adding if statement for boundary so will not go off pg
             if (this.x > 0) {
             this.x -= 101;
@@ -191,7 +153,6 @@ Vise Versa if - is on down it will go up */
 };
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-//var allEnemies = [];
 // Place the player object in a variable called player
 const player = new Player();
 //enemy object for bugs
@@ -202,26 +163,13 @@ const player = new Player();
 const bug1 = new Enemy(-101, 0, 150);
 const bug2 = new Enemy(-101, 75, 125);
 const bug3 = new Enemy(-101, 170, 200);
-//const bug4 = new Enemy(-101, 125, 100);
+//const bug4 = new Enemy(-101, 125, 100);//made browser crash so too out for now
 const allEnemies = [];
-//to push bugs into array -- took out bug for now to test
+//to push bugs into array -- took out bug4 for now
 allEnemies.push(bug1,bug2,bug3);
 console.log(allEnemies);
 
-/* Having prob with game stopping so trying to move this into
-player constructor***
-
-//adding gameOver function to stop game
-function gameOver() {
-    //allEnemies = [];
-    //player.x = this.startX;
-    id = win.requestAnimationFrame(main);  
-    player.winGame = true;
-    player.x = this.startX;
-    player.y = this.startY;
-}
-*/
-//gm over modal from sweetalerts 
+//gm over modal from sweetalerts2 
 function gmModal() {
     swal({
         title: 'Game Over',
@@ -233,10 +181,6 @@ function gmModal() {
           location.reload();
       })
 }
-
-
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
